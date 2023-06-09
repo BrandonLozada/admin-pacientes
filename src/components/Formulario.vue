c30a300612b38904430c13cebd27f2beaa23e752
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-09 15:27:00 -0600

    Implementar función de agregar paciente a la lista de pacientes

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,152 +1,145 @@
 <script setup>
-import { reactive } from 'vue'
-import Alerta from './Alerta.vue'
+import { reactive } from "vue";
+import Alerta from "./Alerta.vue";
 
 const alerta = reactive({
-    tipo: '',
-    mensaje: ''
-})
+  tipo: "",
+  mensaje: "",
+});
 
-defineEmits(['update:nombre', 'update:propietario', 'update:email', 'update:alta', 'update:sintomas'])
+const emit = defineEmits([
+  "update:nombre",
+  "update:propietario",
+  "update:email",
+  "update:alta",
+  "update:sintomas",
+  "guardar-paciente",
+]);
 
 const props = defineProps({
-    nombre: {
-        type: String,
-        required: true
-    },
-    propietario: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    alta: {
-        type: String,
-        required: true
-    },
-    sintomas: {
-        type: String,
-        required: true
-    }
-})
+  nombre: {
+    type: String,
+    required: true,
+  },
+  propietario: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  alta: {
+    type: String,
+    required: true,
+  },
+  sintomas: {
+    type: String,
+    required: true,
+  },
+});
 
 const validar = () => {
-    if (Object.values(props).includes('')) {
-        alerta.mensaje = 'Todos los campos son requeridos'
-        alerta.tipo = 'error'
-        return
-    }
-}
+  if (Object.values(props).includes("")) {
+    alerta.mensaje = "Todos los campos son requeridos";
+    alerta.tipo = "error";
+    return;
+  }
+
+  emit('guardar-paciente')
+};
 </script>
 
 <template>
-    <div class="md:w-1/2">
-        <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-        <p class="text-lg mt-5 text-center mb-10">
-            Añade Pacientes y
-            <span class="text-indigo-600 font-bold">Adminístralos</span>
-        </p>
+  <div class="md:w-1/2">
+    <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+    <p class="text-lg mt-5 text-center mb-10">
+      Añade Pacientes y
+      <span class="text-indigo-600 font-bold">Adminístralos</span>
+    </p>
 
-        <Alerta
-            v-if="alerta.mensaje"
-            :alerta="alerta"
+    <Alerta v-if="alerta.mensaje" :alerta="alerta" />
+    <form
+      class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      @submit.prevent="validar"
+    >
+      <div class="mb-5">
+        <label for="mascota" class="block text-gray-700 uppercase font-bold">
+          Nombre Mascota
+        </label>
+        <input
+          id="mascota"
+          type="text"
+          placeholder="Nombre de la mascota"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          :value="nombre"
+          @input="$emit('update:nombre', $event.target.value)"
         />
-        <form
-            class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
-            @submit.prevent="validar"
+      </div>
+
+      <div class="mb-5">
+        <label
+          for="propietario"
+          class="block text-gray-700 uppercase font-bold"
         >
-            <div class="mb-5">
-                <label
-                    for="mascota"
-                    class="block text-gray-700 uppercase font-bold"
-                >
-                    Nombre Mascota
-                </label>
-                <input
-                    id="mascota"
-                    type="text"
-                    placeholder="Nombre de la mascota"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    :value="nombre"
-                    @input="$emit('update:nombre', $event.target.value)"
-                />
-            </div>
+          Nombre Propietario
+        </label>
+        <input
+          id="propietario"
+          type="text"
+          placeholder="Nombre del propietario"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          :value="propietario"
+          @input="$emit('update:propietario', $event.target.value)"
+        />
+      </div>
 
-            <div class="mb-5">
-                <label
-                    for="propietario"
-                    class="block text-gray-700 uppercase font-bold"
-                >
-                    Nombre Propietario
-                </label>
-                <input
-                    id="propietario"
-                    type="text"
-                    placeholder="Nombre del propietario"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    :value="propietario"
-                    @input="$emit('update:propietario', $event.target.value)"
-                />
-            </div>
+      <div class="mb-5">
+        <label for="email" class="block text-gray-700 uppercase font-bold">
+          Email
+        </label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Email del propietario"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          :value="email"
+          @input="$emit('update:email', $event.target.value)"
+        />
+      </div>
 
-            <div class="mb-5">
-                <label
-                    for="email"
-                    class="block text-gray-700 uppercase font-bold"
-                >
-                    Email
-                </label>
-                <input
-                    id="email"
-                    type="email"
-                    placeholder="Email del propietario"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    :value="email"
-                    @input="$emit('update:email', $event.target.value)"
-                />
-            </div>
+      <div class="mb-5">
+        <label for="alta" class="block text-gray-700 uppercase font-bold">
+          Alta
+        </label>
+        <input
+          id="alta"
+          type="date"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          :value="alta"
+          @input="$emit('update:alta', $event.target.value)"
+        />
+      </div>
 
-            <div class="mb-5">
-                <label
-                    for="alta"
-                    class="block text-gray-700 uppercase font-bold"
-                >
-                    Alta
-                </label>
-                <input
-                    id="alta"
-                    type="date"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    :value="alta"
-                    @input="$emit('update:alta', $event.target.value)"
-                />
-            </div>
+      <div class="mb-5">
+        <label for="sintomas" class="block text-gray-700 uppercase font-bold">
+          Síntomas
+        </label>
+        <textarea
+          id="sintomas"
+          type="text"
+          placeholder="Describe los síntomas de la mascota"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+          :value="sintomas"
+          @input="$emit('update:sintomas', $event.target.value)"
+        />
+      </div>
 
-            <div class="mb-5">
-                <label
-                    for="sintomas"
-                    class="block text-gray-700 uppercase font-bold"
-                >
-                    Síntomas
-                </label>
-                <textarea
-                    id="sintomas"
-                    type="text"
-                    placeholder="Describe los síntomas de la mascota"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
-                    :value="sintomas"
-                    @input="$emit('update:sintomas', $event.target.value)"
-                />
-            </div>
-
-            <input
-                type="submit"
-                class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-                value="Registrar paciente"
-            />
-
-        </form>
-    </div>
+      <input
+        type="submit"
+        class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+        value="Registrar paciente"
+      />
+    </form>
+  </div>
 </template>
